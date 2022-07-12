e5efb4c0457378946cb655c7e9e136b56583307d
Steve ChoCho <cyk3233@gmail.com>
2022-07-12 13:59:55 +0900

    Terraform AWS Module S3

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "region" {
+    description = "AWS Region"
+    value = var.region
+}
+
+output "s3_bucket_name" {
+    description = "AWS S3 Bucket Name"
+    value = aws_s3_bucket.this.bucket
+}
+
+output "dynamodb_table" {
+    description = "AWS DynamoDB Table Name"
+    value = aws_dynamodb_table.this.name
+}
+
+output "kms_key" {
+    description = "AWS KMS KEY"
+    value = aws_kms_key.this.arn
+}
